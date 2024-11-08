72e90357c699ae70bc6f590359dbbc3144902d16
AkifhanIlgaz <akifhanilgaz@gmail.com>
2024-11-08 05:49:08 +0300

    add .gitignore file

--- queries/words/words.sql
+++ queries/words/words.sql
@@ -1,7 +1,7 @@
+-- Active: 1731033646568@@127.0.0.1@5432@oxford-5000@public
 CREATE TABLE words (
     id SERIAL PRIMARY KEY,
     word TEXT NOT NULL,
     uk_audio TEXT,
-    us_audio TEXT,
-    
+    us_audio TEXT
 )
